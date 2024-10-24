7ca163eb6e4025e4dee7ba2e5e3b3456ce45362b
jamtechdev <jamtechdev@gmail.com>
2024-10-24 17:33:28 +0530

    addng_file

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ import cors from "cors";
 import { BASE_PORT } from "./utlis/config.js";
 import { startCronJobs } from "./cronJobs.js";
 import logger from "./_helpers/logger.js";
+import path from "path";
 import transcriptionRoutes from "./service/transcription.js";
 const app = express();
 app.use(cors());
-app.use(express.json()); // Parse incoming JSON requests
+app.use(express.json()); 
 app.use(
   "/service/generated_images",
   express.static(path.join(__dirname, "generated_images"))
 );
-app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests (if needed)
+app.use(express.urlencoded({ extended: true }));
 app.use("/", transcriptionRoutes);
 app.listen(BASE_PORT, () => {
   logger.info(`Proxy server running at http://localhost:${BASE_PORT}`);
